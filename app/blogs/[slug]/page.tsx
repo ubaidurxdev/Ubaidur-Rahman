31cf907f9805a40f04aee7238e5df2aaaf86655e
noob-ubaid <ubaidurrahman661@gmail.com>
2025-12-25 00:10:10 +0600

    updated blogs page

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -1,22 +1,51 @@
-import React from "react";
-import Markdown from "markdown-to-jsx";
 import fs from "fs";
 import matter from "gray-matter";
 import { notFound } from "next/navigation";
+import rehypeDocument from "rehype-document";
+import rehypeFormat from "rehype-format";
+import rehypeStringify from "rehype-stringify";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import { unified } from "unified";
+import rehypePrettyCode from "rehype-pretty-code";
+import { transformerCopyButton } from "@rehype-pretty/transformers";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 
-const Page = async({ params }: { params: { slug: string } }) => {
-  const { slug } = await params 
-  const filePath = `content/${slug}.md`
-  if(!fs.existsSync(filePath)) {
-    return notFound()
+const Page = async ({ params }: { params: { slug: string } }) => {
+  const { slug } = await params;
+  const filePath = `content/${slug}.md`;
+  if (!fs.existsSync(filePath)) {
+    return notFound();
   }
-  const fileContent = fs.readFileSync(filePath,"utf-8")
-  const {content,data} = matter(fileContent)
+  const fileContent = fs.readFileSync(filePath, "utf-8");
+  const { content } = matter(fileContent);
+  const processor = unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeDocument, { title: "👋🌍" })
+    .use(rehypeFormat)
+    .use(rehypeStringify)
+    .use(rehypeSlug)
+    .use(rehypeAutolinkHeadings)
+    .use(rehypePrettyCode, {
+      theme: "github-dark",
+      transformers: [
+        transformerCopyButton({
+          visibility: "always",
+          feedbackDuration: 3_000,
+        }),
+      ],
+    });
+
+  const htmlContent = (await processor.process(content)).toString();
   return (
     <main className="mt-14">
       <article>
-        <h4 className="text-2xl font-semibold">{data.title}</h4>
-        <p className="mt-4 text-text-color">{data.description}</p>
+        <div
+          dangerouslySetInnerHTML={{ __html: htmlContent }}
+          className="prose dark:prose-invert max-w-none w-full"
+        ></div>
       </article>
     </main>
   );
